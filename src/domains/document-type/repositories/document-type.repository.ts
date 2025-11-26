053408cb7e75eb396cd2d0f3b4629dec4ed42c2f
Willem Franco <willemffrancoc@gmail.com>
2025-11-26 18:10:56 -0500

    Correcciones de eslint y typescript

--- src/domains/document-type/repositories/document-type.repository.ts
+++ src/domains/document-type/repositories/document-type.repository.ts
@@ -1,7 +1,11 @@
-import { Repository } from '@shared/domain/base-entity';
 import { DocumentType } from '../entities/document-type.entity';
 
-export interface DocumentTypeRepository extends Repository<DocumentType> {
+export interface DocumentTypeRepository {
+  findById(id: string): Promise<DocumentType | null>;
+  findAll(): Promise<DocumentType[]>;
+  save(request: Partial<Omit<DocumentType, 'id'>>): Promise<DocumentType>;
+  update(request: DocumentType & { id: string }): Promise<DocumentType>;
+  delete(id: string): Promise<void>;
   findByName(name: string): Promise<DocumentType | null>;
   existsByName(name: string): Promise<boolean>;
 }
